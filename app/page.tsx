e6359b8a1987cebade1347a04eae212f24999e50
Luis Xavier Carcamo Velazquez <luisvelazquez98271@gmail.com>
2024-07-03 15:55:09 -0600

    Hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
+import { FloatingNav } from "@/components/FloatingNav";
+import Grid from "@/components/Grid";
+import Hero from "@/components/Hero";
 import Image from "next/image";
+import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <main className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-10">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <h1>Xavier</h1>
+        <h1>
+          <FloatingNav navItems={[
+            { name: 'Home', link: '/', icon: <FaHome />}
+          ]} />
+          <Hero />
+          <Grid />
+        </h1>
       </div>
     </main>
    );
