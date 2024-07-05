ca974144ceb3c2eebf487947f9d5ed3fbd614c81
Xavier Velazquez <luisvelazquez98271@gmail.com>
2024-07-04 18:59:09 -0600

    Added projects section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { FloatingNav } from "@/components/FloatingNav";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import RecentProjects from "@/components/RecentProjects";
+import { navItems } from "@/data";
 import Image from "next/image";
 import { FaHome } from "react-icons/fa";
 
@@ -9,13 +11,12 @@ export default function Home() {
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <h1>
-          <FloatingNav navItems={[
-            { name: 'Home', link: '/', icon: <FaHome />}
-          ]} />
+          <FloatingNav navItems={navItems} />
           <Hero />
           <Grid />
+          <RecentProjects />
         </h1>
       </div>
     </main>
-   );
+  );
 }
